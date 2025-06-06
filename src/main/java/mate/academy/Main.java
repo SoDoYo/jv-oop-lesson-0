0c6d40274a14cd1ec002aba4758c73343db13931
Danylo Kurtiak <zxxh127@gmail.com>
2025-06-06 03:24:11 +0200

    test

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -4,14 +4,14 @@ import mate.academy.service.AuthenticationService;
 
 public class Main {
     private static final AuthenticationService authenticationService = new AuthenticationService();
-
+    
     public static void main(String[] args) {
         test("bob@i.ua", "1234", true);
         test("alice@i.ua", "1234", true);
         test("john@i.ua", "1234", false);
         test("bob@i.ua", "qwerty", false);
     }
-    
+
     private static void test(String email, String password, boolean expected) {
         boolean actual = authenticationService.login(email, password);
         if (expected == actual) {
